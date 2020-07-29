f2ec56cb7d579c7e23dde4c0841c07c26e077dd5
aytami <68582141+aytami@users.noreply.github.com>
2020-07-29 11:55:07 +0800

    Add files via upload

---
+++ firebase/form.js
@@ -0,0 +1,149 @@
+// Your web app's Firebase configuration
+
+  var firebaseConfig = {
+  
+    apiKey: "AIzaSyCjwbKUvDopzXFnl8KhYeYHj0YGhrSHUjI",
+    authDomain: "mydatabase-c1f53.firebaseapp.com",
+    databaseURL: "https://mydatabase-c1f53.firebaseio.com",
+    projectId: "mydatabase-c1f53",
+    storageBucket: "mydatabase-c1f53.appspot.com",
+    messagingSenderId: "925452085527",
+    appId: "1:925452085527:web:85300750c3f19de643af52",
+    measurementId: "G-KSE2N84FVH"
+  };
+
+  // Initialize Firebase
+
+  firebase.initializeApp(firebaseConfig);
+
+ 
+
+        const auth = firebase.auth();
+
+        
+
+        
+
+        function signUp(){
+
+                
+
+                var email = document.getElementById("email");
+
+                var password = document.getElementById("password");
+
+                
+
+                const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+
+                promise.catch(e => alert(e.message));
+
+                
+
+                alert("Signed Up");
+
+        }
+
+        
+
+        
+
+        
+
+        function signIn(){
+
+                
+
+                var email = document.getElementById("email");
+
+                var password = document.getElementById("password");
+
+                
+
+                const promise = auth.signInWithEmailAndPassword(email.value, password.value);
+
+                promise.catch(e => alert(e.message));
+
+                
+
+                
+
+                
+
+                
+
+        }
+
+        
+
+        
+
+        function signOut(){
+
+                
+
+                auth.signOut();
+
+                alert("Signed Out");
+
+                
+
+        }
+
+        
+
+        
+
+        
+
+        auth.onAuthStateChanged(function(user){
+
+                
+
+                if(user){
+
+                        
+
+                         // User is signed in
+                         
+                        document.getElementById("user_div").style.display = "block";
+                        document.getElementById("login_div").style.display = "none";
+
+                        var user = firebase.auth().currentUser;
+
+                        if(user != null){
+
+                        var email_id = user.email;
+                        document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+
+
+                        alert("Active User " + email);
+
+                        
+
+                        //Take user to a different or home page
+
+
+                        //is signed in
+
+                        
+
+                }else{
+
+                        document.getElementById("user_div").style.display = "none";
+                        document.getElementById("login_div").style.display = "block";
+
+
+                        alert("No Active User");
+
+                        //no user is signed in
+
+                }
+
+                
+
+                
+
+                
+
+        });
