9888526a37c0dd6ad153b327e0546529a20dd216
aytami <68582141+aytami@users.noreply.github.com>
2020-07-29 12:33:33 +0800

    Create auth.js

---
+++ firebase/auth.js
@@ -0,0 +1,25 @@
+// Your web app's Firebase configuration
+
+  var firebaseConfig = {
+  
+    apiKey: "AIzaSyCjwbKUvDopzXFnl8KhYeYHj0YGhrSHUjI",
+    authDomain: "mydatabase-c1f53.firebaseapp.com",
+    databaseURL: "https://mydatabase-c1f53.firebaseio.com",
+    projectId: "mydatabase-c1f53",
+    storageBucket: "mydatabase-c1f53.appspot.com",
+    messagingSenderId: "925452085527",
+    appId: "1:925452085527:web:85300750c3f19de643af52",
+    measurementId: "G-KSE2N84FVH"
+  };
+
+  // Initialize Firebase
+
+  firebase.initializeApp(firebaseConfig);
+
+ 
+
+        const auth = firebase.auth();
+
+        
+
+        
